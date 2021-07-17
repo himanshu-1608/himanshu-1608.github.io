a18c64536e5bdc50ac919caa6b78a5ec77332cab
Himanshu Yadav <ydv1608@gmail.com>
2021-07-17 10:28:57 +0530

    Added reactive hands

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
-let faces = document.querySelectorAll('.face');
+let personList = document.querySelectorAll('.person');
 
+let faceList = Array.from(personList).map((person) => {
+	return person.querySelector('.face');
+});
+let armList = Array.from(personList).map((person) => {
+	return person.querySelectorAll('.arms');
+});
 document.addEventListener('mousemove', (e) => {
-	for (face of faces) {
+	for (let i = 0; i < personList.length; i++) {
+		let face = faceList[i];
 		let faceBoundingRect = face.getBoundingClientRect();
 		if (
 			(faceBoundingRect.top >= 0 &&
@@ -9,6 +16,7 @@ document.addEventListener('mousemove', (e) => {
 			(faceBoundingRect.top < window.innerHeight &&
 				faceBoundingRect.bottom >= 0)
 		) {
+			let arms = armList[i];
 			let faceCenter = {
 				x: faceBoundingRect.left + faceBoundingRect.width / 2,
 				y: faceBoundingRect.top + faceBoundingRect.height / 2,
@@ -18,10 +26,16 @@ document.addEventListener('mousemove', (e) => {
 				(180 / Math.PI);
 			if (angle >= 0 && angle <= 180) {
 				face.style.transform = `rotate(${angle - 90}deg)`;
+				for (arm of arms) {
+					arm.style.transform = `rotate(${angle - 90}deg)`;
+				}
 			}
 
 			if (angle > -180 && angle < 0) {
 				face.style.transform = `rotate(${angle + 90}deg) scale(-1,1)`;
+				for (arm of arms) {
+					arm.style.transform = `rotate(${angle - 90}deg)`;
+				}
 			}
 		}
 	}
