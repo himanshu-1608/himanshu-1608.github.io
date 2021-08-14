b4209c729584962bfe6e1107e3d4ee9bb63b4616
Himanshu Yadav <ydv1608@gmail.com>
2021-08-14 12:32:40 +0530

    Add dialog on window resize

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-const heightPercent = window.innerHeight / 100;
-const widthPercent = window.innerWidth / 100;
+let heightPercent = window.innerHeight / 100;
+let widthPercent = window.innerWidth / 100;
+
+const reloadFunction = (e) => location.reload();
+
+let isResized = false;
+window.addEventListener('resize', function (event) {
+	if (!isResized) {
+		resizeDialog = document.querySelector('.resize-dialog');
+		resizeDialog.style.display = 'flex';
+		isResized = true;
+		reloadButton = document.getElementById('reload-button');
+		reloadButton.addEventListener('click', reloadFunction);
+	}
+});
